49b0d18aa8d6f5d072de015b8fd461f50ab647c6
Vjs Pranavasri <pranavasri@live.in>
2021-08-27 01:15:08 +0530

    Update sleepy_staging.sh

--- sleepy_staging.sh
+++ sleepy_staging.sh
@@ -24,9 +24,9 @@ green='\e[0;32m'
 # Deleting out "kernel complied" and zip "anykernel" from an old compilation
 echo -e "$green << cleanup >> \n $white"
 
-rm -rf out
-rm -rf zip
-rm -rf error.log
+# rm -rf out
+# rm -rf zip
+# rm -rf error.log
 
 echo -e "$green << setup dirs >> \n $white"
 
@@ -52,8 +52,6 @@ if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 #
 # TOOLCHAIN = the toolchain u want to use "gcc/clang"
 
-export CHATID API_BOT
-
 DEVICE="Redmi Note 4/4X"
 CODENAME="mido"
 KERNEL_NAME="SleepyKernel"
@@ -63,153 +61,150 @@ KRNL_REL_TAG="v7"
 
 DEFCONFIG="mido_defconfig"
 
-AnyKernel="https://github.com/shashank1436/anykernel"
-AnyKernelbranch="master"
-
-HOSST="sleeping-bag"
-USEER="shashank"
-
-TOOLCHAIN="clang"
-
 # setup telegram env
-export BOT_MSG_URL="https://api.telegram.org/bot$API_BOT/sendMessage"
-export BOT_BUILD_URL="https://api.telegram.org/bot$API_BOT/sendDocument"
+BOT_MSG_URL="https://api.telegram.org/bot$API_BOT/sendMessage"
+BOT_BUILD_URL="https://api.telegram.org/bot$API_BOT/sendDocument"
 
-tg_post_msg() {
-        curl -s -X POST "$BOT_MSG_URL" -d chat_id="$2" \
-        -d "parse_mode=html" \
-        -d text="$1"
-}
+echo $BOT_MSG_URL
+echo $BOT_BUILD_URL
+echo $API_BOT
+echo $CHATID
 
-tg_post_build() {
-        #Post MD5Checksum alongwith for easeness
-        MD5CHECK=$(md5sum "$1" | cut -d' ' -f1)
+# tg_post_msg() {
+#         curl -s -X POST "$BOT_MSG_URL" -d chat_id="$2" \
+#         -d "parse_mode=html" \
+#         -d text="$1"
+# }
 
-        #Show the Checksum alongwith caption
-        curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
-        -F chat_id="$2" \
-        -F "disable_web_page_preview=true" \
-        -F "parse_mode=html" \
-        -F caption="$3 build finished in $(($Diff / 60)) minutes and $(($Diff % 60)) seconds | <b>MD5 Checksum : </b><code>$MD5CHECK</code>"
-}
+# tg_post_build() {
+#         #Post MD5Checksum alongwith for easeness
+#         MD5CHECK=$(md5sum "$1" | cut -d' ' -f1)
 
-tg_error() {
-        curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
-        -F chat_id="$2" \
-        -F "disable_web_page_preview=true" \
-        -F "parse_mode=html" \
-        -F caption="$3Failed to build , check <code>error.log</code>"
-}
+#         #Show the Checksum alongwith caption
+#         curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
+#         -F chat_id="$2" \
+#         -F "disable_web_page_preview=true" \
+#         -F "parse_mode=html" \
+#         -F caption="$3 build finished in $(($Diff / 60)) minutes and $(($Diff % 60)) seconds | <b>MD5 Checksum : </b><code>$MD5CHECK</code>"
+# }
 
-# Now let's clone gcc/clang on HOME dir
-# And after that , the script start the compilation of the kernel it self
-# For regen the defconfig . use the regen.sh script
+# tg_error() {
+#         curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
+#         -F chat_id="$2" \
+#         -F "disable_web_page_preview=true" \
+#         -F "parse_mode=html" \
+#         -F caption="$3Failed to build , check <code>error.log</code>"
+# }
 
-if [ "$TOOLCHAIN" == gcc ]; then
-	if [ ! -d "$HOME/gcc64" ] && [ ! -d "$HOME/gcc32" ]
-	then
-		echo -e "$green << cloning gcc from arter >> \n $white"
-		git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 "$HOME"/gcc64
-		git clone --depth=1 https://github.com/mvaisakh/gcc-arm "$HOME"/gcc32
-	fi
-	export PATH="$HOME/gcc64/bin:$HOME/gcc32/bin:$PATH"
-	export STRIP="$HOME/gcc64/aarch64-elf/bin/strip"
-	export KBUILD_COMPILER_STRING=$("$HOME"/gcc64/bin/aarch64-elf-gcc --version | head -n 1)
-elif [ "$TOOLCHAIN" == clang ]; then
-	if [ ! -d "$HOME/proton_clang" ]
-	then
-		echo -e "$green << cloning proton clang >> \n $white"
-		git clone --depth=1 https://github.com/kdrag0n/proton-clang.git "$HOME"/proton_clang
-	fi
-	export PATH="$HOME/proton_clang/bin:$PATH"
-	export STRIP="$HOME/proton_clang/aarch64-linux-gnu/bin/strip"
-	export KBUILD_COMPILER_STRING=$("$HOME"/proton_clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-fi
+# # Now let's clone gcc/clang on HOME dir
+# # And after that , the script start the compilation of the kernel it self
+# # For regen the defconfig . use the regen.sh script
 
-# Setup build process
+# if [ "$TOOLCHAIN" == gcc ]; then
+# 	if [ ! -d "$HOME/gcc64" ] && [ ! -d "$HOME/gcc32" ]
+# 	then
+# 		echo -e "$green << cloning gcc from arter >> \n $white"
+# 		git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 "$HOME"/gcc64
+# 		git clone --depth=1 https://github.com/mvaisakh/gcc-arm "$HOME"/gcc32
+# 	fi
+# 	export PATH="$HOME/gcc64/bin:$HOME/gcc32/bin:$PATH"
+# 	export STRIP="$HOME/gcc64/aarch64-elf/bin/strip"
+# 	export KBUILD_COMPILER_STRING=$("$HOME"/gcc64/bin/aarch64-elf-gcc --version | head -n 1)
+# elif [ "$TOOLCHAIN" == clang ]; then
+# 	if [ ! -d "$HOME/proton_clang" ]
+# 	then
+# 		echo -e "$green << cloning proton clang >> \n $white"
+# 		git clone --depth=1 https://github.com/kdrag0n/proton-clang.git "$HOME"/proton_clang
+# 	fi
+# 	export PATH="$HOME/proton_clang/bin:$PATH"
+# 	export STRIP="$HOME/proton_clang/aarch64-linux-gnu/bin/strip"
+# 	export KBUILD_COMPILER_STRING=$("$HOME"/proton_clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+# fi
 
-build_kernel() {
-Start=$(date +"%s")
+# # Setup build process
 
-if [ "$TOOLCHAIN" == clang  ]; then
-	echo clang
-	make -j$(nproc --all) O=out \
-                              ARCH=arm64 \
-                              AR=llvm-ar \
-                              NM=llvm-nm \
-                              OBJCOPY=llvm-objcopy \
-                              OBJDUMP=llvm-objdump \
-                              STRIP=llvm-strip \
-                              CC=clang \
-                              CROSS_COMPILE=aarch64-linux-gnu- \
-                              CROSS_COMPILE_ARM32=arm-linux-gnueabi-  2>&1 | tee error.log
-elif [ "$TOOLCHAIN" == gcc  ]; then
-	echo gcc
-	make -j$(nproc --all) O=out \
-			      ARCH=arm64 \
-			      CROSS_COMPILE=aarch64-elf- \
-			      CROSS_COMPILE_ARM32=arm-eabi- 2>&1 | tee error.log
-fi
+# build_kernel() {
+# Start=$(date +"%s")
 
-End=$(date +"%s")
-Diff=$(($End - $Start))
-}
+# if [ "$TOOLCHAIN" == clang  ]; then
+# 	echo clang
+# 	make -j$(nproc --all) O=out \
+#                               ARCH=arm64 \
+#                               AR=llvm-ar \
+#                               NM=llvm-nm \
+#                               OBJCOPY=llvm-objcopy \
+#                               OBJDUMP=llvm-objdump \
+#                               STRIP=llvm-strip \
+#                               CC=clang \
+#                               CROSS_COMPILE=aarch64-linux-gnu- \
+#                               CROSS_COMPILE_ARM32=arm-linux-gnueabi-  2>&1 | tee error.log
+# elif [ "$TOOLCHAIN" == gcc  ]; then
+# 	echo gcc
+# 	make -j$(nproc --all) O=out \
+# 			      ARCH=arm64 \
+# 			      CROSS_COMPILE=aarch64-elf- \
+# 			      CROSS_COMPILE_ARM32=arm-eabi- 2>&1 | tee error.log
+# fi
 
-export IMG="$MY_DIR"/out/arch/arm64/boot/Image.gz-dtb
+# End=$(date +"%s")
+# Diff=$(($End - $Start))
+# }
 
-# Let's start
+# export IMG="$MY_DIR"/out/arch/arm64/boot/Image.gz-dtb
 
-echo -e "$green << doing pre-compilation process >> \n $white"
-export ARCH=arm64
-export SUBARCH=arm64
-export HEADER_ARCH=arm64
+# # Let's start
 
-export KBUILD_BUILD_HOST="$HOSST"
-export KBUILD_BUILD_USER="$USEER"
+# echo -e "$green << doing pre-compilation process >> \n $white"
+# export ARCH=arm64
+# export SUBARCH=arm64
+# export HEADER_ARCH=arm64
 
-mkdir -p out
+# export KBUILD_BUILD_HOST="$HOSST"
+# export KBUILD_BUILD_USER="$USEER"
 
-make O=out clean && make O=out mrproper
-make "$DEFCONFIG" O=out
+# mkdir -p out
 
-echo -e "$yellow << compiling the kernel >> \n $white"
-tg_post_msg "<code>Compiling kernel</code>" "$CHATID"
+# make O=out clean && make O=out mrproper
+# make "$DEFCONFIG" O=out
 
-build_kernel || error=true
+# echo -e "$yellow << compiling the kernel >> \n $white"
+# tg_post_msg "<code>Compiling kernel</code>" "$CHATID"
 
-DATE=$(date +"%Y%m%d-%H%M%S")
-KERVER=$(make kernelversion)
+# build_kernel || error=true
 
-        if [ -f "$IMG" ]; then
-                echo -e "$green << Build completed in $(($Diff / 60)) minutes and $(($Diff % 60)) seconds >> \n $white"
-        else
-                echo -e "$red << Failed to compile the kernel , Check up to find the error >>$white"
-                tg_post_msg "Kernel failed to compile uploading error log"
-                tg_error "error.log" "$CHATID"
-                rm -rf out
-                rm -rf testing.log
-                rm -rf error.log
-                exit 1
-        fi
+# DATE=$(date +"%Y%m%d-%H%M%S")
+# KERVER=$(make kernelversion)
 
-        if [ -f "$IMG" ]; then
-                echo -e "$green << cloning AnyKernel from your repo >> \n $white"
-                git clone "$AnyKernel" --single-branch -b "$AnyKernelbranch" zip
-                echo -e "$yellow << making kernel zip >> \n $white"
-                cp -r "$IMG" zip/
-                cd zip
-                mv Image.gz-dtb zImage
-                export ZIP="$KERNEL_NAME"-"$KRNL_REL_TAG"-Stagging-"$CODENAME"-"$DATE"
-                zip -r9 "$ZIP" * -x .git README.md LICENSE *placeholder
-                curl -sLo zipsigner-3.0.jar https://raw.githubusercontent.com/shashank1436/anykernel/zipper/zipsigner-3.0.jar
-                java -jar zipsigner-3.0.jar "$ZIP".zip "$ZIP"-signed.zip
-                tg_post_msg "Kernel successfully compiled uploading ZIP" "$CHATID"
-                tg_post_build "$ZIP"-signed.zip "$CHATID"
-                cd ..
-                rm -rf error.log
-                rm -rf out
-                rm -rf zip
-                rm -rf testing.log
-                exit
-        fi
+#         if [ -f "$IMG" ]; then
+#                 echo -e "$green << Build completed in $(($Diff / 60)) minutes and $(($Diff % 60)) seconds >> \n $white"
+#         else
+#                 echo -e "$red << Failed to compile the kernel , Check up to find the error >>$white"
+#                 tg_post_msg "Kernel failed to compile uploading error log"
+#                 tg_error "error.log" "$CHATID"
+#                 rm -rf out
+#                 rm -rf testing.log
+#                 rm -rf error.log
+#                 exit 1
+#         fi
+
+#         if [ -f "$IMG" ]; then
+#                 echo -e "$green << cloning AnyKernel from your repo >> \n $white"
+#                 git clone "$AnyKernel" --single-branch -b "$AnyKernelbranch" zip
+#                 echo -e "$yellow << making kernel zip >> \n $white"
+#                 cp -r "$IMG" zip/
+#                 cd zip
+#                 mv Image.gz-dtb zImage
+#                 export ZIP="$KERNEL_NAME"-"$KRNL_REL_TAG"-Stagging-"$CODENAME"-"$DATE"
+#                 zip -r9 "$ZIP" * -x .git README.md LICENSE *placeholder
+#                 curl -sLo zipsigner-3.0.jar https://raw.githubusercontent.com/shashank1436/anykernel/zipper/zipsigner-3.0.jar
+#                 java -jar zipsigner-3.0.jar "$ZIP".zip "$ZIP"-signed.zip
+#                 tg_post_msg "Kernel successfully compiled uploading ZIP" "$CHATID"
+#                 tg_post_build "$ZIP"-signed.zip "$CHATID"
+#                 cd ..
+#                 rm -rf error.log
+#                 rm -rf out
+#                 rm -rf zip
+#                 rm -rf testing.log
+#                 exit
+#         fi
 
